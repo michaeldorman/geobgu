214f494338b2eed4095fc71673db16e468cff43c
michaeldorman <michael.dorman@mail.huji.ac.il>
2019-06-02 11:47:16 +0300

    v0.2.0

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' US states
+#'
+#' Polygonal layer with US states
+#'
+#' @format An \code{sf} layer of type \code{POLYGON}
+#' @examples
+#' data(states)
+
+"states"
+
+#' US cities
+#'
+#' Point layer with US cities
+#'
+#' @format An \code{sf} layer of type \code{POINT}
+#' @examples
+#' data(cities)
+
+"cities"
+
---
+++ R/simpleCap.R
@@ -0,0 +1,27 @@
+#' Simple word capitalization
+#'
+#' @param x A character vector
+#'
+#' @return A capitalized character vector
+#'
+#' @references
+#' \url{https://stackoverflow.com/questions/6364783/capitalize-the-first-letter-of-both-words-in-a-two-word-string}
+#'
+#' @export
+#'
+#' @examples
+#' name = c("zip code", "state", "final count")
+#' simpleCap(name)
+
+simpleCap = function(x) {
+  sapply(x, function(x) {
+    s = strsplit(x, " ")[[1]]
+    paste(
+      toupper(substring(s, 1, 1)),
+      tolower(substring(s, 2)),
+      sep = "",
+      collapse = " "
+      )
+  })
+}
+
